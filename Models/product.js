bb3117423a7d46b36c5b3be193092b0f8da8503d
Juan Jose Caimen Cespedes <32856@pycomunicaciones.local>
2023-03-06 14:00:53 -0300

    commit schema mongoose

---
+++ Models/product.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  productCode: { type: String, require: true },
+  title: { type: String, require: true },
+  imageUrl: { type: String, require: true },
+  pirce: { type: Number, require: true },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
