1eaa5fd8926d0636a887794faf4189e11ed87cca
Juan <jjcaimen@gmail.com>
2023-03-10 22:19:50 -0300

    commit

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,6 +1,5 @@
 exports.errorHanlder = (err) => {
   if (!err.isEmpty()) {
-    console.log(err);
     const error = new Error("Validation failed");
     error.statusCode = 422;
     error.message = "Error al ingresar un dato";
